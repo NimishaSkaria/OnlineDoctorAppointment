86935c300068aeaed5ea2c5c13b92244c0bd242e
NimishaSkaria <58441513+NimishaSkaria@users.noreply.github.com>
2019-12-03 00:02:35 +0530

    Add files via upload

---
+++ zuulserver/src/test/java/com/infosys/onlinedoctorappointment/zuul/ZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infosys.onlinedoctorappointment.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
